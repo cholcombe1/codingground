c08efe65266725e59a3bf927a43ea173bbc5d084
cholcombe1 <cholcombe27394@jcss.us>
2015-12-15 13:17:25 -0500

    Created by Coding Ground Client

---
+++ EX71/AverageFinder.java
@@ -0,0 +1,8 @@
+public class AverageFinder
+{
+    // Write your methods here for p.373 Ex 7.1, 7.2,7.3
+    pubic static float average (int num1, int num2)
+    {
+        return ((float) int num1 + int num2) / 2;
+    }
+} 
